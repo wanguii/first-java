797232d05ca68211329276fbe0d98a2936c1102c
wanguii <beatrice.ndonga@admi.ac.kegit config --global user.email beatrice.ndonga@admi.ac.ke>
2018-09-27 09:47:35 -0700

    notified who i am

---
+++ src/beatrice/Beatrice.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beatrice;
+
+/**
+ *
+ * @author hp
+ */
+public class Beatrice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //System.out.println("Hello");
+        // System.out.println("Hello");
+        
+        
+        /**
+       
+        int age = 15;//age will be used for arithmetics\
+        double weight = 70.5;
+        float height = 1.8f;
+        String name = "beatrice";
+        String secondname = "ndonga";
+        char gender = 'm';
+        boolean healthy = true;
+        double bmi = weight/(height*height);
+        if(bmi>20){
+        healthy = false;
+        
+    }
+        
+        System.out.println("I'm " +  name.concat(secondname));*/
+        int x =40;
+        int y =20;
+        double division= x/y;
+        double addition= x+y;
+        double multiplication= x*y;
+        double subtraction= x/y;
+        
+        System.out.println(division);
+         System.out.println(addition);
+          System.out.println(multiplication);
+           System.out.println(subtraction);
+           
+    }   
+    
+   
+    
+}
---
+++ src/beatrice/week3.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beatrice;
+
+/**
+ *
+ * @author hp
+ */
+public class week3 {
+    public static void main(String[] args) {
+        int x=50;
+        int y=200;
+        String name1="wewe";
+         String name2="mimi";
+        boolean check =true;
+        
+        
+        if(x>y){
+            
+            System.out.println("x is greater than y");        
+       
+        }else{
+           System.out.println("x is not greater than y"); 
+        }
+        
+        
+         if(x==y){
+            
+            System.out.println("x equals to y");        
+       
+        }else{
+           System.out.println("x not equal to y"); 
+        }
+        
+         
+                          
+                 
+         
+         
+         
+         
+         if(x!=y){
+            
+            System.out.println("x not equal to y");        
+       
+        }else{
+           System.out.println("x equal to y"); 
+        }
+         
+         
+         
+          if(x>=y){
+            
+            System.out.println("x is greater than or equal  y");        
+       
+        }else{
+           System.out.println("x is not greater than y"); 
+        }
+          
+         
+              
+        if(name1.equals(name2)){
+            
+            System.out.println("name1 is similar to name2");        
+       
+        }else{
+           System.out.println("name1 is not similar to name2"); 
+        }
+          
+         
+         
+        
+        
+        
+        
+        
+         if(check==true){
+            
+            System.out.println("check is true");        
+       
+        }else{
+           System.out.println("check is false"); 
+        }   
+         
+         
+    }
+}
